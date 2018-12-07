8bc62bae71e2e7a74597efdd6074ed7ecde1769d
Maxime <maxime.hetmann@icloud.com>
2018-12-07 09:59:02 +0100

    0.3 responsive

--- js/script.js
+++ js/script.js
@@ -1,48 +1,63 @@
-var p =0;
-var down=90;
-var lock_scroll=0;
-function li_setup() {
-    move_section(0);
-    for (i = 0; i < 4; i++) {
-        b = Math.floor(Math.random() * 1000);
-        test(i, b);
-    }
-}
+if (window.matchMedia("(min-width: 730px)").matches) { // superieur a 730 px
+    var p = 0;
+    var down = 90;
+    var lock_scroll = 0;
 
-window.onload = li_setup;
 
-function test(i, b) {
-    setTimeout(() => {
-        document.getElementsByTagName('li')[i].style.top = '0px';
-    }, b);
-}
-
-function move_section(a) {
-    p=a;
-    for (i = 0; i < 4; i++) {
-        document.getElementsByTagName('section')[i].style = "left:"+(-(100*a))+'vw;top:90px;';
-        lock_scroll=1;
-    }
-    setTimeout(() => {
-        lock_scroll=0;
-    }, 2000);
-}
-
-window.addEventListener("wheel", event => {
-    const delta = Math.sign(event.wheelDelta);
-    if(lock_scroll==0){
-    if(delta==1){
-        // monter
-        down +=80;
-        if(down>90){
-            down=90
+    function li_setup() {   // timeout aleatoire
+        move_section(0);
+        for (i = 0; i < 4; i++) {
+            b = Math.floor(Math.random() * 1000);
+            test(i, b);
         }
-        document.getElementsByTagName('section')[p].style.top = down+'px';
     }
-    else{
-        // descendre
-        down -=80;
-        document.getElementsByTagName('section')[p].style.top = down+'px';
+
+    window.onload = li_setup;
+
+    function test(i, b) {  //mise en position de la liste
+        setTimeout(() => {
+            document.getElementsByTagName('li')[i].style.top = '0px';
+        }, b);
     }
+
+    function move_section(a) {
+        p = a;
+        for (i = 0; i < 4; i++) {
+            document.getElementsByTagName('section')[i].style = "left:" + (-(100 * a)) + 'vw;top:90px;';
+            lock_scroll = 1;
+        }
+        setTimeout(() => {
+            lock_scroll = 0;
+        }, 2000);
+    }
+
+    window.addEventListener("wheel", event => {
+        const delta = Math.sign(event.wheelDelta);
+        if (lock_scroll == 0) {
+            if (delta == 1) {
+                // monter
+                down += 80;
+                if (down > 90) {
+                    down = 90
+                }
+                document.getElementsByTagName('section')[p].style.top = down + 'px';
+            }
+            else {
+                // descendre
+                down -= 80;
+                document.getElementsByTagName('section')[p].style.top = down + 'px';
+            }
+        }
+    });
+
+
+
+
+
+
+} else {   //  ECRAN INFERIEUR A 730 PX
+    
+
+
+
 }
-});
